df860667a052d463e80760e09921620e8ede44cd
Gibson <maxime.vialette@gmail.com>
2023-10-23 20:45:37 +0200

    add new song + mp3

--- lib/providers/songs_provider.dart
+++ lib/providers/songs_provider.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:Metronomy/model/music_structure.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,6 +8,13 @@ class SongsNotifier extends StateNotifier<List<Song>> {
 
   Future<void> loadSongs() async {
 
+    //List<Song> allSongs = <Song>[];
+    List<Song> allSongs = <Song>[getHereComesTheRainAgain()];
+
+    state = allSongs;
+  }
+
+  Song getMyWay(){
     // load one song
     MusicStructure _musicStructureIntro = new MusicStructure(sectionName: "INTRO", sectionShortcut: "I", maximumBarsSection: 16, maximumBeatSection: 4);
     MusicStructure _musicStructureVerse = new MusicStructure(sectionName: "VERSE", sectionShortcut: "V", maximumBarsSection: 16, maximumBeatSection: 4);
@@ -25,11 +31,45 @@ class SongsNotifier extends StateNotifier<List<Song>> {
     myWaySteps.add(_musicStructureChorus);
     myWaySteps.add(_musicStructureOutro);
 
-    Song myCurrentSong = new Song(title: "My Way", tempo: 222, musiquePart: myWaySteps);
+    return new Song(title: "My Way", tempo: 222, musiquePart: myWaySteps);
+  }
 
-    List<Song> allSongs = <Song>[myCurrentSong];
+  // Here Comes The Rain Again
+  Song getHereComesTheRainAgain(){
 
-    state = allSongs;
+/*    Couplet : 8 mesures x2
+    Refrain 16 mesures
+    Couplet
+    Refrain
+    Pont : 8 mesures x 2
+    Refrain
+    Intro
+    Couplet
+    Couplet*/
+
+    // load one song
+    MusicStructure _musicStructureIntro = new MusicStructure(sectionName: "INTRO", sectionShortcut: "I", maximumBarsSection: 8, maximumBeatSection: 4);
+    MusicStructure _musicStructureCouplet = new MusicStructure(sectionName: "COUPLET", sectionShortcut: "C", maximumBarsSection: 8, maximumBeatSection: 4);
+    MusicStructure _musicStructureRefrain = new MusicStructure(sectionName: "REFRAIN", sectionShortcut: "R", maximumBarsSection: 16, maximumBeatSection: 4);
+    MusicStructure _musicStructurePont = new MusicStructure(sectionName: "PONT", sectionShortcut: "P", maximumBarsSection: 8, maximumBeatSection: 4);
+
+    List<MusicStructure> musicSteps =  <MusicStructure>[];
+    musicSteps.add(_musicStructureIntro);
+    musicSteps.add(_musicStructureIntro);
+    musicSteps.add(_musicStructureCouplet);
+    musicSteps.add(_musicStructureCouplet);
+    musicSteps.add(_musicStructureRefrain);
+    musicSteps.add(_musicStructureCouplet);
+    musicSteps.add(_musicStructureRefrain);
+    musicSteps.add(_musicStructurePont);
+    musicSteps.add(_musicStructurePont);
+    musicSteps.add(_musicStructureRefrain);
+    musicSteps.add(_musicStructureIntro);
+    musicSteps.add(_musicStructureIntro);
+    musicSteps.add(_musicStructureCouplet);
+    musicSteps.add(_musicStructureCouplet);
+
+    return new Song(title: "Here comes the rain again", tempo: 115, musiquePart: musicSteps);
   }
 }
 

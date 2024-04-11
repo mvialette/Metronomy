a311c2682d1b769d6550e5456731f8f524aa586d
Gibson <maxime.vialette@gmail.com>
2024-04-11 14:00:32 +0200

    add latency feature

--- metronomy-mobile/lib/screen/music_player_screen.dart
+++ metronomy-mobile/lib/screen/music_player_screen.dart
@@ -1,19 +1,20 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-import 'package:metronomy/model/song.dart';
-import 'package:metronomy/providers/settings_notifier.dart';
-import 'package:metronomy/store/rhythm_provider.dart';
-import 'package:metronomy/store/rhythm_store.dart';
-import 'package:metronomy/ui/sound_toggle_button.dart';
-import 'package:metronomy/ui/stop_button.dart';
-import 'package:metronomy/widgets/bullets.dart';
-import 'package:metronomy/widgets/bullets_countdown.dart';
+import 'package:Metronomy/model/song.dart';
+import 'package:Metronomy/providers/settings_notifier.dart';
+import 'package:Metronomy/screen/home_screen.dart';
+import 'package:Metronomy/store/rhythm_provider.dart';
+import 'package:Metronomy/store/rhythm_store.dart';
+import 'package:Metronomy/ui/sound_toggle_button.dart';
+import 'package:Metronomy/ui/stop_button.dart';
+import 'package:Metronomy/widgets/bullets.dart';
+import 'package:Metronomy/widgets/bullets_countdown.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MusicPlayerScreen extends ConsumerStatefulWidget {
+
   const MusicPlayerScreen({super.key, required this.onSelectScreen});
 
   final void Function(String identifier) onSelectScreen;
@@ -33,136 +34,136 @@ class _MusicPlayerScreenState extends ConsumerState<MusicPlayerScreen> {
     super.initState();
   }
 
-  // ignore: unused_element
   void _printSong(Song currentSong) {
-    JsonEncoder encoder = const JsonEncoder.withIndent('  ');
-    if (kDebugMode) {
-      print(encoder.convert(currentSong));
-    }
+    JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+    print(encoder.convert(currentSong));
   }
 
-  Column getPlayWidgets() {
+  Column getPlayWidgets(){
     return Column(children: [
       Visibility(
         visible: ref.read(allSettingsProvider).debuggingMode,
         child: Column(
           children: [
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text('${RhythmStore.of(context).debugTickCount}',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyLarge!
-                      .copyWith(color: Theme.of(context).colorScheme.secondary))
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(AppLocalizations.of(context)!.debugHitCount,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall!
-                      .copyWith(color: Theme.of(context).colorScheme.primary))
-            ]),
-            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '${RhythmStore.of(context).debugTickCount}',
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  AppLocalizations.of(context)!.debugHitCount,
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40,),
           ],
         ),
       ),
       Visibility(
-          visible: ref.read(allSettingsProvider).advanceMode,
-          child: Column(children: [
-            const SizedBox(height: 30),
+        visible: ref.read(allSettingsProvider).advanceMode,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
             Text(
               AppLocalizations.of(context)!.breadcrumb,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            SizedBox(
-                height: 50,
-                child: ListView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: RhythmProvider.of(context)
+            Container(
+              height: 50,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: RhythmProvider.of(context)
+                    .selectedSong
+                    .musiquePart
+                    .length,
+                itemBuilder: (BuildContext context, int index) => Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  child: CircleAvatar(
+                    backgroundColor:
+                    RhythmStore.of(context).sectionCurrentIndex == index
+                        ? Colors.orange
+                        : Colors.grey,
+                    child: Text(RhythmProvider.of(context)
                         .selectedSong
-                        .musiquePart
-                        .length,
-                    itemBuilder: (BuildContext context, int index) => Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        child: CircleAvatar(
-                            backgroundColor:
-                                RhythmStore.of(context).sectionCurrentIndex ==
-                                        index
-                                    ? Colors.orange
-                                    : Colors.grey,
-                            child: Text(RhythmProvider.of(context)
-                                .selectedSong
-                                .musiquePart[index]
-                                .sectionShortcut)))))
-          ])),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
+                        .musiquePart[index]
+                        .sectionShortcut),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
             '${RhythmStore.of(context).barsCurrentCounter} / ${RhythmProvider.of(context).selectedSong.musiquePart[RhythmStore.of(context).sectionCurrentIndex].maximumBarsSection}',
-            style: Theme.of(context)
-                .textTheme
-                .bodyLarge!
-                .copyWith(color: Theme.of(context).colorScheme.secondary))
-      ]),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(AppLocalizations.of(context)!.beatsByBar,
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall!
-                .copyWith(color: Theme.of(context).colorScheme.primary))
-      ]),
-      const SizedBox(height: 30),
-      const SizedBox(height: 50, child: Bullets()),
-      const SizedBox(height: 30),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-            RhythmProvider.of(context)
-                .selectedSong
-                .musiquePart[RhythmStore.of(context).sectionCurrentIndex]
-                .sectionName,
-            style: Theme.of(context)
-                .textTheme
-                .bodyLarge!
-                .copyWith(color: Theme.of(context).colorScheme.secondary))
-      ]),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(AppLocalizations.of(context)!.songSection,
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall!
-                .copyWith(color: Theme.of(context).colorScheme.primary))
-      ]),
-      const SizedBox(height: 30),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-            RhythmProvider.of(context)
-                .selectedSong
-                .nextSectionName(RhythmStore.of(context).sectionCurrentIndex),
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall!
-                .copyWith(color: Theme.of(context).colorScheme.secondary))
-      ]),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(AppLocalizations.of(context)!.nextSection,
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall!
-                .copyWith(color: Theme.of(context).colorScheme.primary))
-      ]),
-      const SizedBox(height: 30),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(RhythmProvider.of(context).rhythm.toString(),
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            AppLocalizations.of(context)!.beatsByBar,
             style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                  color: Theme.of(context).colorScheme.secondary,
-                ))
-      ]),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(AppLocalizations.of(context)!.tempo,
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall!
-                .copyWith(color: Theme.of(context).colorScheme.primary))
-      ])
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 30,
+      ),
+      Container(
+        height: 50,
+        child: Bullets(),
+      ),
+
+      SizedBox(
+        height: 30,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            RhythmProvider.of(context).rhythm.toString(),
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            AppLocalizations.of(context)!.tempo,
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
     ]);
   }
 
@@ -177,85 +178,127 @@ class _MusicPlayerScreenState extends ConsumerState<MusicPlayerScreen> {
     //   );
     // }
 
-    SoundToggleButton soundToggleButton =
-        SoundToggleButton(setStateCallback: () {
-      setState(() {});
-    });
+    SoundToggleButton soundToggleButton = SoundToggleButton(
+      setStateCallback: () {
+        setState(() {});
+      },
+    );
 
-    return Column(children: [
-      Row(
-        children: [
+    return Column(
+      children: [
+        Row(children: [
           IconButton(
-              icon: Icon(Icons.arrow_back,
-                  color: Theme.of(context).colorScheme.primary),
-              tooltip: 'Select this song en go to play mode',
-              onPressed: () {
-                widget.onSelectScreen('all-songs');
-              }),
-          Expanded(child: Container())
-        ],
-      ),
-      Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Icon(Icons.music_note,
-                //Icons.music_note,
-                color: Theme.of(context).colorScheme.primary,
-                size: 40),
-            const SizedBox(height: 10),
-            Text(
-              RhythmProvider.of(context).selectedSong.title,
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
+            icon: Icon(
+              Icons.arrow_back,
+              color: Theme.of(context).colorScheme.primary,
             ),
-            const SizedBox(height: 10),
-            Text('Author',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodySmall!
-                    .copyWith(color: Theme.of(context).colorScheme.primary))
-          ])),
-      Visibility(
-          visible: RhythmProvider.of(context).startingCountdown > 0,
-          child: Column(children: [
-            const SizedBox(height: 40),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text('${RhythmProvider.of(context).startingCountdown - 1}',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyLarge!
-                      .copyWith(color: Theme.of(context).colorScheme.secondary))
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(AppLocalizations.of(context)!.countdown,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall!
-                      .copyWith(color: Theme.of(context).colorScheme.primary))
-            ]),
-            const SizedBox(height: 20),
-            Column(children: [
+            tooltip: 'Select this song en go to play mode',
+            onPressed: () {
+              widget.onSelectScreen('all-songs');
+            },
+          ),
+          Expanded(
+            child: Container(),
+          ),
+        ],),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               SizedBox(
+                height: 10,
+              ),
+              Text(
+                RhythmProvider.of(context).selectedSong.title,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${RhythmProvider.of(context).selectedSong.musiquePart[RhythmStore.of(context).sectionCurrentIndex].sectionName}',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.songSection,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Visibility(
+          visible: RhythmProvider.of(context).startingCountdown > 0,
+          child: Column(
+            children: [
+              SizedBox(height: 40,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${RhythmProvider.of(context).startingCountdown - 1}',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.countdown,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Column(children: [
+                Container(
                   height: 50,
                   //child: BulletsCountdown(ref.read(allSettingsProvider).startingBarsNumber),
-                  child: BulletsCountdown(
-                      startingBarsNumber:
-                          ref.read(allSettingsProvider).startingBarsNumber))
-            ])
-          ])),
-      const SizedBox(height: 30),
-      RhythmProvider.of(context).startingCountdown > 0
-          ? const Text("")
-          : getPlayWidgets(),
-      Expanded(child: Container()),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        soundToggleButton,
-        const SizedBox(width: 8.0),
-        StopButton(setStateCallback: () {
-          setState(() {});
-        })
-      ])
-    ]);
+                  child: BulletsCountdown(startingBarsNumber: ref.read(allSettingsProvider).startingBarsNumber,),
+                ),
+              ],)
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        RhythmProvider.of(context).startingCountdown > 0 ? Text("") : getPlayWidgets(),
+        Expanded(child: Container()),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            soundToggleButton,
+            const SizedBox(width: 8.0),
+            StopButton(
+              setStateCallback: () {
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
--- metronomy-mobile/lib/ui/sound_toggle_button.dart
+++ metronomy-mobile/lib/ui/sound_toggle_button.dart
@@ -1,16 +1,16 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
-import 'package:metronomy/providers/settings_notifier.dart';
-import 'package:metronomy/store/rhythm_provider.dart';
+import 'package:Metronomy/providers/settings_notifier.dart';
+import 'package:Metronomy/store/rhythm_provider.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:metronomy/model/constants.dart';
+import 'package:Metronomy/model/constants.dart';
 
-import 'package:metronomy/store/rhythm_store.dart';
+import 'package:Metronomy/store/rhythm_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SoundToggleButton extends ConsumerStatefulWidget {
+
   final Function setStateCallback;
 
   final AudioPlayer audioPlayerHighPitchedSound = AudioPlayer()
@@ -25,13 +25,14 @@ class SoundToggleButton extends ConsumerStatefulWidget {
   SoundToggleButton({super.key, required this.setStateCallback});
 
   static Duration getRhythmInterval(int rhythm) =>
-      Duration(microseconds: (((60 / rhythm) * 1000) * 1000).toInt());
+      Duration(microseconds: (((60 / rhythm) * 1000)* 1000).toInt());
 
   @override
   ConsumerState<SoundToggleButton> createState() => _SoundToggleButtonState();
 }
 
 class _SoundToggleButtonState extends ConsumerState<SoundToggleButton> {
+
   Timer? periodicTimer;
   int oldValuePrint = 0;
 
@@ -39,8 +40,8 @@ class _SoundToggleButtonState extends ConsumerState<SoundToggleButton> {
 
   @override
   void dispose() {
-    widget.audioPlayerHighPitchedSound.dispose();
-    widget.audioPlayerLowPitchedSound.dispose();
+    this.widget.audioPlayerHighPitchedSound.dispose();
+    this.widget.audioPlayerLowPitchedSound.dispose();
 
     periodicTimer?.cancel();
     super.dispose();
@@ -48,8 +49,8 @@ class _SoundToggleButtonState extends ConsumerState<SoundToggleButton> {
 
   @override
   Widget build(BuildContext context) {
-    tempoDuration =
-        SoundToggleButton.getRhythmInterval(RhythmProvider.of(context).rhythm);
+
+    tempoDuration = SoundToggleButton.getRhythmInterval(RhythmProvider.of(context).rhythm) as Duration;
 
     return ElevatedButton(
       //enableFeedback: false,
@@ -59,8 +60,7 @@ class _SoundToggleButtonState extends ConsumerState<SoundToggleButton> {
       // ),
       onPressed: () {
         setState(() {
-          RhythmProvider.of(context)
-              .updateEnableTimer(!RhythmStore.of(context).enable);
+          RhythmProvider.of(context).updateEnableTimer(!RhythmStore.of(context).enable);
         });
       },
       //tooltip: 'Play',
@@ -73,83 +73,84 @@ class _SoundToggleButtonState extends ConsumerState<SoundToggleButton> {
 
   @override
   void didChangeDependencies() {
-    tempoDuration =
-        SoundToggleButton.getRhythmInterval(RhythmProvider.of(context).rhythm);
+
+    tempoDuration = SoundToggleButton.getRhythmInterval(RhythmProvider.of(context).rhythm) as Duration;
 
     periodicTimer?.cancel();
     periodicTimer = Timer.periodic(
       // récupérer la valeur courante du rythme
       tempoDuration!,
-      (_) {
-        final bool firstSongDifferent =
-            ref.read(allSettingsProvider).firstSongDifferent;
-        //final bool firstSongDifferent = true;
-        _printMaintenant();
+          (_) {
+            final bool firstSongDifferent = ref.read(allSettingsProvider).firstSongDifferent;
+            //final bool firstSongDifferent = true;
+            _printMaintenant();
 
-        if (RhythmStore.of(context).enable) {
-          RhythmProvider.of(context).updateMakeCountdown();
+            if (RhythmStore.of(context).enable) {
 
-          if (RhythmProvider.of(context).startingCountdown > 0) {
-            playLowSound();
-          } else if (RhythmStore.of(context).debugTickCount >= 0) {
-            if (firstSongDifferent &&
-                RhythmProvider.of(context).debugTickCount %
-                        RhythmProvider.of(context).selectedSong.beatsByBar ==
-                    0) {
-              // case occures when this is the first tick as mesure / bar (aka modulo == 0)
-              playHighSound();
-            } else {
-              // case occures when startingCountdown == 0, debugTickCount > 0 (ie : time 2, 3, 4, 6, 7, 8, 10, 11, 12, 14, ...)
-              playLowSound();
-            }
-          } else {
-            if (RhythmProvider.of(context).startingCountdown == 0 &&
-                RhythmProvider.of(context).debugTickCount > 0) {
-              // case occures when startingCountdown == 0, debugTickCount = 0 (time 1)
-              if (firstSongDifferent) {
-                playHighSound();
-              } else {
+              RhythmProvider.of(context).updateMakeCountdown();
+
+              if(RhythmProvider.of(context).startingCountdown >0){
                 playLowSound();
+              }else if (RhythmStore.of(context).debugTickCount >= 0) {
+                if(firstSongDifferent && RhythmProvider.of(context).debugTickCount % RhythmProvider.of(context).selectedSong.beatsByBar == 0){
+                  // case occures when this is the first tick as mesure / bar (aka modulo == 0)
+                  playHighSound();
+                }else{
+                  // case occures when startingCountdown == 0, debugTickCount > 0 (ie : time 2, 3, 4, 6, 7, 8, 10, 11, 12, 14, ...)
+                  playLowSound();
+                }
+              }else {
+                if(RhythmProvider.of(context).startingCountdown == 0 && RhythmProvider.of(context).debugTickCount > 0) {
+                  // case occures when startingCountdown == 0, debugTickCount = 0 (time 1)
+                  if(firstSongDifferent){
+                    playHighSound();
+                  } else {
+                    playLowSound();
+                  }
+                } else {
+                  // case occures when startingCountdown > 0, debugTickCount = 0 (time -9, -8, -7, -6, -5, -4, -3, -2, -1, 0)
+                  playLowSound();
+                }
               }
-            } else {
-              // case occures when startingCountdown > 0, debugTickCount = 0 (time -9, -8, -7, -6, -5, -4, -3, -2, -1, 0)
-              playLowSound();
             }
-          }
-        }
-        widget.setStateCallback();
+            this.widget.setStateCallback();
       },
     );
     super.didChangeDependencies();
   }
 
+
   void playLowSound() {
-    widget.audioPlayerLowPitchedSound.pause();
-    widget.audioPlayerLowPitchedSound.seek(Duration.zero);
-    widget.audioPlayerLowPitchedSound.play(songLow);
+
+    Future.delayed(const Duration(milliseconds: 200), () {
+      this.widget.audioPlayerLowPitchedSound.pause();
+      this.widget.audioPlayerLowPitchedSound.seek(Duration.zero);
+      this.widget.audioPlayerLowPitchedSound.play(songLow);
+    });
   }
 
   void playHighSound() {
-    widget.audioPlayerHighPitchedSound.pause();
-    widget.audioPlayerHighPitchedSound.seek(Duration.zero);
-    widget.audioPlayerHighPitchedSound.play(songHigh);
+
+    Future.delayed(const Duration(milliseconds: 200), () {
+      this.widget.audioPlayerHighPitchedSound.pause();
+      this.widget.audioPlayerHighPitchedSound.seek(Duration.zero);
+      this.widget.audioPlayerHighPitchedSound.play(songHigh);
+    });
   }
 
   void _printMaintenant() {
+
     var nowDT = DateTime.now();
     var nowMicrosecondsSinceEpoch = nowDT.microsecondsSinceEpoch;
     int gradian = nowMicrosecondsSinceEpoch - oldValuePrint;
 
-    if (kDebugMode) {
-      print('$nowDT // ${gradian / 1000} microsec');
-    }
+    print('${nowDT} // ${gradian /1000} microsec');
 
     oldValuePrint = nowMicrosecondsSinceEpoch;
   }
 
-  void refreshTempoDuration(BuildContext context) {
-    tempoDuration =
-        SoundToggleButton.getRhythmInterval(RhythmProvider.of(context).rhythm);
+  void refreshTempoDuration(BuildContext context){
+    tempoDuration = SoundToggleButton.getRhythmInterval(RhythmProvider.of(context).rhythm);
     RhythmProvider.of(context).updateStopTimer();
   }
 }

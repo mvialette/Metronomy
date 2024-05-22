e1e40364beb49064e87a2f7e403cce466545cd79
Gibson <maxime.vialette@gmail.com>
2024-05-22 22:06:28 +0200

    correction bug getLocalFile

--- metronomy-mobile/lib/screen/song_list_screen.dart
+++ metronomy-mobile/lib/screen/song_list_screen.dart
@@ -4,17 +4,21 @@ import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
 
-import 'package:metronomy/model/constants.dart';
-import 'package:metronomy/model/song.dart';
-import 'package:metronomy/store/rhythm_provider.dart';
-import 'package:metronomy/widgets/song_item.dart';
+import 'package:Metronomy/model/constants.dart';
+import 'package:Metronomy/model/song.dart';
+import 'package:Metronomy/store/rhythm_provider.dart';
+import 'package:Metronomy/widgets/song_item.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SongListScreen extends StatelessWidget {
-  const SongListScreen({super.key, required this.onSelectScreen});
+
+  const SongListScreen({
+    super.key,
+    required this.onSelectScreen
+  });
 
   final void Function(String identifier) onSelectScreen;
 
@@ -27,24 +31,18 @@ class SongListScreen extends StatelessWidget {
   Future<File> getLocalFile(String collectionDatabaseName) async {
     final path = await _localPath;
 
-    late String collectionDatabaseNameInLocal;
-    Directory(path).listSync().forEach((fileIntoApplicationDocumentsDirectory) {
-      String currentFileName =
-          Directory(path).listSync()[2].path.split('/').last;
-      if (currentFileName.startsWith("database-songs_20")) {
+    String collectionDatabaseNameInLocal = "";
+    Directory('$path').listSync().forEach((fileIntoApplicationDocumentsDirectory) {
+      String currentFileName = fileIntoApplicationDocumentsDirectory.path.split('/').last;
+      if(currentFileName.startsWith("database-songs_20")){
         collectionDatabaseNameInLocal = currentFileName;
       }
-      //allAvailableSongsInLocal.add(Song.fromMap(songString));
     });
 
-    // ignore: unnecessary_null_comparison
-    return File('$path/database-$collectionDatabaseNameInLocal' == null
-        ? '$collectionDatabaseName.json'
-        : collectionDatabaseNameInLocal);
+    return File('$path/' + (collectionDatabaseNameInLocal.isEmpty ? 'database-' + collectionDatabaseName + '.json' : collectionDatabaseNameInLocal));
   }
 
-  Future<File> writeDatabase(
-      String collectionDatabaseName, List<Song> songs) async {
+  Future<File> writeDatabase(String collectionDatabaseName, List<Song> songs) async {
     final file = await getLocalFile(collectionDatabaseName);
 
     String songsInJsonString = jsonEncode(songs);
@@ -72,18 +70,17 @@ class SongListScreen extends StatelessWidget {
     }
   }
 
-  Future<List<Song>> getAllAvailableSongsFromFirestore(
-      String collectionDatabaseName) async {
-    CollectionReference songs =
-        FirebaseFirestore.instance.collection(collectionDatabaseName);
+  Future<List<Song>> getAllAvailableSongsFromFirestore(String collectionDatabaseName) async {
+
+    CollectionReference songs = FirebaseFirestore.instance.collection(collectionDatabaseName);
     List<Song> allAvailableSongs = <Song>[];
 
-    await songs.get().then((QuerySnapshot snapshot) {
-      for (var doc in snapshot.docs) {
-        allAvailableSongs.add(Song.fromMap(doc.data() as Map<String, dynamic>));
-      }
-      // ignore: invalid_return_type_for_catch_error, avoid_print
-    }).catchError((error) => print("Failed to fetch users: $error"));
+     await songs.get().then((QuerySnapshot snapshot) {
+       snapshot.docs.forEach((doc) {
+         allAvailableSongs.add(Song.fromMap(doc.data() as Map<String, dynamic>));
+       });
+
+     }).catchError((error) => print("Failed to fetch users: $error"));
 
     // save into a local file
     writeDatabase(collectionDatabaseName, allAvailableSongs);
@@ -91,30 +88,26 @@ class SongListScreen extends StatelessWidget {
     return allAvailableSongs;
   }
 
-  Future<List<Song>> getAllAvailableSongsFromLocalFile(
-      String collectionDatabaseName) async {
+  Future<List<Song>> getAllAvailableSongsFromLocalFile(String collectionDatabaseName) async {
     List<Song> allAvailableSongs = await readDatabase(collectionDatabaseName);
     return allAvailableSongs;
   }
 
   Future<List<Song>> getAllAvailableSongs() async {
-    CollectionReference settings =
-        FirebaseFirestore.instance.collection(kFirebaseSettings);
+
+    CollectionReference settings = FirebaseFirestore.instance.collection(kFirebaseSettings);
     late String collectionDatabaseName;
 
-    // ignore: unused_local_variable
     List<List<String>> allFirebaseSettings = <List<String>>[];
     await settings.get().then((QuerySnapshot snapshot) {
-      for (var doc in snapshot.docs) {
+      snapshot.docs.forEach((doc) {
         collectionDatabaseName = (doc.data()! as Map)["name"];
-      }
-    }).catchError((error) =>
-        // ignore: invalid_return_type_for_catch_error, avoid_print
-        print("Failed to fetch database settings from Firebase: $error"));
+      });
+
+    }).catchError((error) => print("Failed to fetch database settings from Firebase: $error"));
 
     //List<Song> allAvailableSongs = await getAllAvailableSongsFromLocalFile(collectionDatabaseName);
-    List<Song> allAvailableSongs =
-        await getAllAvailableSongsFromFirestore(collectionDatabaseName);
+    List<Song> allAvailableSongs = await getAllAvailableSongsFromFirestore(collectionDatabaseName);
 
     return allAvailableSongs;
   }
@@ -122,57 +115,67 @@ class SongListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User? user = FirebaseAuth.instance.currentUser;
-    // ignore: unused_local_variable
     final name = user != null ? user.displayName : "toto";
-    // ignore: unused_local_variable
     final userEmail = user != null ? user.email : "toto";
 
     return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: FutureBuilder<List<Song>>(
-            future: getAllAvailableSongs(),
-            builder: (
-              BuildContext context,
-              AsyncSnapshot<List<Song>> snapshot,
+      padding: const EdgeInsets.all(8.0),
+      child: FutureBuilder<List<Song>>(
+        future: getAllAvailableSongs(),
+        builder: (
+            BuildContext context,
+            AsyncSnapshot<List<Song>> snapshot,
             ) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return const Text('Error');
-                } else if (snapshot.hasData) {
-                  return Column(children: [
-                    const SizedBox(height: 15),
-                    Text(AppLocalizations.of(context)!.listDescriptionAllSongs,
-                        style: Theme.of(context).textTheme.bodyMedium),
-                    const SizedBox(height: 15),
-                    ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: snapshot.data?.length,
-                        itemBuilder: (ctx, index) => Column(children: [
-                              SongItem(
-                                  song: snapshot.data![index],
-                                  onSelectSong: (song) {
-                                    RhythmProvider.of(context)
-                                        .updateSong(song, index);
-                                    onSelectScreen('summary');
-                                  }),
-                              const Divider(
-                                height: 20,
-                                thickness: 1,
-                                indent: 20,
-                                endIndent: 0,
-                                color: Colors.grey,
-                              )
-                            ]))
-                  ]);
-                } else {
-                  return const Text('Empty data');
-                }
-              } else {
-                return Text('State: ${snapshot.connectionState}');
-              }
-            }));
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return const Text('Error');
+            } else if (snapshot.hasData) {
+
+              return Column(
+                children: [
+                  SizedBox(height: 15,),
+                  Text(
+                    AppLocalizations.of(context)!.listDescriptionAllSongs,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+
+                  SizedBox(height: 15,),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (ctx, index) =>
+                      Column(
+                        children: [
+                          SongItem(
+                            song: snapshot.data![index],
+                            onSelectSong: (song) {
+                              RhythmProvider.of(context).updateSong(song, index);
+                              onSelectScreen('summary');
+                            },
+                          ),
+                          const Divider(
+                            height: 20,
+                            thickness: 1,
+                            indent: 20,
+                            endIndent: 0,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                  ),
+                ],
+              );
+            } else {
+              return const Text('Empty data');
+            }
+          } else {
+            return Text('State: ${snapshot.connectionState}');
+          }
+        },
+      ),
+    );
   }
 }
